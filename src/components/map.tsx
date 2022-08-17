bd5514992be4415cfd34d4415739429a687e488b
louisdutton <contact@louisdutton.com>
2022-08-17 09:29:04 +0100

    removed un-tested features and refactor

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -6,7 +6,6 @@ import {
   createContext,
   useContext,
   createUniqueId,
-  untrack,
   Accessor,
   Show,
   JSX,
@@ -20,49 +19,28 @@ type TransitionType = "flyTo" | "easeTo" | "jumpTo";
 type ContainerProps = "id" | "class" | "classList" | "ref" | "children";
 type MapEventHandlers = MappedEventHandlers<mapboxgl.MapEventType>;
 
-export type Viewport = {
-  id?: string;
-  center?: mapboxgl.LngLatLike;
-  bounds?: mapboxgl.LngLatBounds;
-  zoom?: number;
-  pitch?: number;
-  bearing?: number;
-};
-
 // interface MapOptions extends Omit<MBX.MapboxOptions, "container">, Partial<MapEventHandlers> {
 //   style?: MapStyle;
 // }
 
 export interface MapProps extends Pick<ComponentProps<"div">, ContainerProps>, Partial<MapEventHandlers> {
   style?: ComponentProps<"div">["style"];
-  /** Current Map View */
-  viewport: Viewport;
   /** Mapbox Options
    * @see https://docs.mapbox.com/mapbox-gl-js/api/map/#map-parameters
    */
   options?: Omit<mapboxgl.MapboxOptions, "container">;
-  /** Type for pan, move and zoom transitions */
-  transitionType?: TransitionType;
-  /** Event listener for Viewport updates */
-  onViewportChange?: (viewport: Viewport) => void;
-  /** Displays Map Tile Borders */
-  showTileBoundaries?: boolean;
-  /** Displays Wireframe if Terrain is visible */
-  showTerrainWireframe?: boolean;
-  /** Displays Borders if Padding is set */
-  showPadding?: boolean;
-  /** Displays Label Collision Boxes */
-  showCollisionBoxes?: boolean;
-  /** Displays all feature outlines even if normally not drawn by style rules */
-  showOverdrawInspector?: boolean;
   /** Mouse Cursor Style */
   cursorStyle?: string;
-  /** Dark Map Style */
-  darkStyle?: mapboxgl.StyleOptions | string;
   /** Disable automatic map resize */
   disableResize?: boolean;
-  /** Debug Mode */
-  debug?: boolean;
+  /** Debug feature toggle */
+  debug?: {
+    tileBoundaries?: boolean;
+    terrainWireframe?: boolean;
+    padding?: boolean;
+    collisionBoxes?: boolean;
+    overdrawInspector?: boolean;
+  };
 }
 
 const MapContext = createContext((() => {}) as Accessor<mapboxgl.Map>);
@@ -73,7 +51,6 @@ export const useMap = () => useContext(MapContext);
 export const MapBox: Component<MapProps> = (props) => {
   props.id = props.id || createUniqueId();
   const [mapbox, setMapbox] = createSignal<mapboxgl.Map>();
-  const [transitionType, setTransitionType] = createSignal<TransitionType>("flyTo");
 
   const debug = (text: string, value: unknown) => props.debug && console.debug(`${text}: %c${value}`, "color: #00F");
 
@@ -86,38 +63,30 @@ export const MapBox: Component<MapProps> = (props) => {
   // };
 
   onMount(() => {
-    const map = new mapboxgl.Map({
-      ...props.options,
-      ...props.viewport,
-      container,
-      interactive: !!props.onViewportChange,
-      // fitBoundsOptions: { padding: props.viewport?.padding },
-    });
-
+    const map = new mapboxgl.Map({ ...props.options, container });
     map.once("load").then(() => setMapbox(map));
 
     // Listen to map container size changes
-    const resizeObserver = new ResizeObserver(() => map.resize());
-    createEffect(() =>
-      props.disableResize ? resizeObserver.disconnect() : resizeObserver.observe(container as Element)
-    );
+    const resizer = new ResizeObserver(() => map.resize());
+    createEffect(() => (props.disableResize ? resizer.disconnect() : resizer.observe(container)));
 
     // Hook up events
     createEffect(() => {
       (Object.keys(props).filter((key) => key.startsWith("on")) as (keyof MapProps)[]).forEach((key) => {
         const event = key.slice(2).toLowerCase() as keyof mapboxgl.MapEventType;
         const callback = props[key] as any;
-        map.on(event, callback);
+        props[key] !== undefined ? map.on(event, callback) : map.off(event, callback);
         onCleanup(() => map.off(event, callback));
       });
     });
 
     // Update debug features
     createEffect(() => {
-      map.showTileBoundaries = props.showTileBoundaries ?? false;
-      map.showTerrainWireframe = props.showTerrainWireframe ?? false;
-      map.showPadding = props.showPadding ?? false;
-      map.showCollisionBoxes = props.showCollisionBoxes ?? false;
+      if (!props.debug) return;
+      map.showTileBoundaries = props.debug.tileBoundaries ?? false;
+      map.showTerrainWireframe = props.debug.terrainWireframe ?? false;
+      map.showPadding = props.debug.padding ?? false;
+      map.showCollisionBoxes = props.debug.collisionBoxes ?? false;
     });
 
     // Update cursor
@@ -127,22 +96,6 @@ export const MapBox: Component<MapProps> = (props) => {
       map.getCanvas().style.cursor = props.cursorStyle;
       return props.cursorStyle;
     });
-
-    //Update transition type
-    createEffect((prev) => {
-      if (props.transitionType === prev || props.transitionType === undefined) return;
-      debug("Update Transition to", props.transitionType);
-      setTransitionType(props.transitionType);
-      return props.transitionType;
-    });
-
-    // Update projection
-    // createEffect((prev?: MBX.MapboxOptions["projection"]) => {
-    //   if (props.options?.projection === prev || !props.options?.projection) return;
-    //   debug("Update Projection to", props.options.projection.name);
-    //   map.setProjection(props.options.projection);
-    //   return props.options.projection;
-    // });
 
     // Update map style
     createEffect((prev) => {
@@ -171,50 +124,6 @@ export const MapBox: Component<MapProps> = (props) => {
       });
       return style;
     }, props.options?.style);
-
-    // Hook up viewport events
-    createEffect(() => {
-      const viewport: Viewport = {
-        id: undefined,
-        center: map.getCenter(),
-        zoom: map.getZoom(),
-        pitch: map.getPitch(),
-        bearing: map.getBearing(),
-        // padding: props.viewport.padding,
-        bounds: props.viewport.bounds,
-      };
-
-      const onMove = (event: mapboxgl.MapboxEvent) => {
-        if (event.originalEvent) props.onViewportChange && props.onViewportChange({ ...viewport, id: props.id });
-        setTransitionType("jumpTo");
-      };
-
-      const onMoveEnd = (event: mapboxgl.MapboxEvent) => {
-        if (event.originalEvent) props.onViewportChange && props.onViewportChange(viewport);
-        if (props.transitionType) setTransitionType(props.transitionType);
-      };
-
-      map.on("move", onMove).on("moveend", onMoveEnd);
-      onCleanup(() => map.off("move", onMove).off("moveend", onMoveEnd));
-    });
-
-    // Update boundaries
-    createEffect((prev) => {
-      if (props.viewport?.bounds && props.viewport?.bounds != prev && props.onViewportChange)
-        props.onViewportChange({
-          ...props.viewport,
-          ...map.cameraForBounds(props.viewport.bounds, {
-            // padding: props.viewport.padding,
-          }),
-        });
-      return props.viewport?.bounds;
-    }, props.viewport?.bounds);
-
-    // Update Viewport
-    createEffect(() => {
-      if (props.id === props.viewport?.id) return;
-      map.stop()[untrack(transitionType)](props.viewport);
-    });
   });
 
   return (
